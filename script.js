164c6cb4be7f173db0b3d78b360482c360a4aed0
hbbhattarai <hemchettri56.geo@gmail.com>
2023-10-13 12:25:25 +0600

    ADD

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.querySelector(".modal");
-  modal.style.display = "none"; // Hide the modal initially
+document.addEventListener('DOMContentLoaded', function() {
+  const modal = document.querySelector('.modal');
+  modal.style.display = 'none'; // Hide the modal initially
 
-  const cells = document.querySelectorAll(".cell");
-  const winnerModal = document.querySelector(".modal");
-  const winnerText = document.querySelector(".winner");
-  const restartButton = document.querySelector(".restart");
+  const cells = document.querySelectorAll('.cell');
+  const winnerModal = document.querySelector('.modal');
+  const winnerText = document.querySelector('.winner');
+  const restartButton = document.querySelector('.restart');
 
   let xTurn = true;
-  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  let gameBoard = ['', '', '', '', '', '', '', '', ''];
   let gameActive = true;
+  let timer; // Initialize a timer variable
 
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+  // Define the time limit (in milliseconds)
+  const timeLimit = 10000; // 30 seconds
 
+  // Function to start the timer
+  function startTimer() {
+    timer = setTimeout(function() {
+      if (gameActive) {
+        endGame(false, true); // Announce winner (timeout)
+      }
+    }, timeLimit);
+  }
+
+  // Function to reset the timer
+  function resetTimer() {
+    clearTimeout(timer);
+    startTimer();
+  }
+
+  // Event listener for cell clicks
   function handleCellClick(event) {
+    resetTimer(); // Reset the timer when a move is made
     const cell = event.target;
     const index = Array.from(cells).indexOf(cell);
 
-    if (gameBoard[index] === "" && gameActive) {
-      gameBoard[index] = xTurn ? "X" : "O";
+    if (gameBoard[index] === '' && gameActive) {
+      gameBoard[index] = xTurn ? 'X' : 'O';
       cell.textContent = gameBoard[index];
-      cell.classList.add(xTurn ? "x" : "o");
+      cell.classList.add(xTurn ? 'x' : 'o');
 
       if (checkWin()) {
         endGame(false);
@@ -44,11 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function checkWin() {
     for (const pattern of winPatterns) {
       const [a, b, c] = pattern;
-      if (
-        gameBoard[a] &&
-        gameBoard[a] === gameBoard[b] &&
-        gameBoard[a] === gameBoard[c]
-      ) {
+      if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
         winnerText.textContent = `Winner is ${gameBoard[a]}`;
         return true;
       }
@@ -57,47 +63,58 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function isDraw() {
-    return !gameBoard.includes("");
+    return !gameBoard.includes('');
   }
 
-  function endGame(draw) {
-    if (!draw) {
+  function endGame(draw, timeout = false) {
+    if (timeout) {
+      winnerText.textContent = 'Player took too long. Opponent wins!';
+    } else if (draw) {
+      winnerText.textContent = 'Game is Draw';
+    } else {
       for (const pattern of winPatterns) {
         const [a, b, c] = pattern;
-        if (
-          gameBoard[a] &&
-          gameBoard[a] === gameBoard[b] &&
-          gameBoard[a] === gameBoard[c]
-        ) {
-          cells[a].style.backgroundColor = "green";
-          cells[b].style.backgroundColor = "green";
-          cells[c].style.backgroundColor = "green";
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+          cells[a].style.backgroundColor = 'green';
+          cells[b].style.backgroundColor = 'green';
+          cells[c].style.backgroundColor = 'green';
           break;
         }
       }
     }
 
-    if (draw) {
-      winnerText.textContent = "Game is Draw";
-    }
-
-    winnerModal.style.display = "block";
+    winnerModal.style.display = 'block';
     gameActive = false;
   }
 
   function restartGame() {
-    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
     gameActive = true;
     xTurn = true;
-    winnerModal.style.display = "none";
-    winnerText.textContent = "";
-    cells.forEach((cell) => {
-      cell.textContent = "";
-      cell.classList.remove("x", "o");
-      cell.style.backgroundColor = "#7777";
+    winnerModal.style.display = 'none';
+    winnerText.textContent = '';
+    cells.forEach(cell => {
+      cell.textContent = '';
+      cell.classList.remove('x', 'o');
+      cell.style.backgroundColor = '#7777';
     });
+    startTimer(); // Start the timer again when the game is restarted
   }
 
-  cells.forEach((cell) => cell.addEventListener("click", handleCellClick));
-  restartButton.addEventListener("click", restartGame);
+  // Define winPatterns...
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  // Define the time limit (in milliseconds)...
+  // const timeLimit = 30000; // 30 seconds
+
+  // Add event listeners for cell clicks and the restart button...
+  cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+  restartButton.addEventListener('click', restartGame);
+
+  // Start the initial timer
+  startTimer();
 });
